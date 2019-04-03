7f976ce022614390cdabe58ad2616fc89a941232
Daniel Wen <daniel.wen@intelliware.com>
2019-04-02 21:55:37 -0400

    section3 basics

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a recipe',
+      'https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/mrtrending0475.jpg?itok=ULkGk3Pn'),
+    new Recipe('A Test Recipe', 'This is a recipe',
+      'https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/mrtrending0475.jpg?itok=ULkGk3Pn'),
+    new Recipe('A Test Recipe', 'This is a recipe',
+      'https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/mrtrending0475.jpg?itok=ULkGk3Pn'),
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
